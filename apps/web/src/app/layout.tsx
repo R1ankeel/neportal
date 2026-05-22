3047f5b31d1e06ae6b2f6d9d67084ba86e6cdb9a
R1ankeel <r1ankeel@gmail.com>
2026-05-22 19:48:35 +0300

    hotfix

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { GeistMono } from "geist/font/mono";
+import { GeistSans } from "geist/font/sans";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Neportal",
@@ -24,7 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <body className={`${geistSans.variable} ${geistMono.variable} min-h-screen antialiased`}>{children}</body>
+      <body className={`${GeistSans.variable} ${GeistMono.variable} min-h-screen antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
