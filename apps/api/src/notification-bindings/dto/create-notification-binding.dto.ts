77903f515755c20b1f50e28f0b76c40cd4b80b73
R1ankeel <r1ankeel@gmail.com>
2026-05-27 15:40:31 +0300

    reply flow fix

--- apps/api/src/notification-bindings/dto/create-notification-binding.dto.ts
+++ apps/api/src/notification-bindings/dto/create-notification-binding.dto.ts
@@ -1,10 +1,29 @@
+import { IsEnum, IsInt, IsOptional, IsString, Min } from "class-validator";
 import { NotificationBindingType } from "@neportal/database";
 
 export class CreateNotificationBindingDto {
+  @IsString()
   telegramChatId!: string;
+
+  @IsInt()
+  @Min(1)
   telegramMessageId!: number;
+
+  @IsString()
   taskId!: string;
+
+  @IsOptional()
+  @IsString()
   sourceCommentId?: string | null;
+
+  @IsOptional()
+  @IsString()
   sourceCommentAuthorId?: string | null;
+
+  @IsEnum(NotificationBindingType)
   notificationType!: NotificationBindingType;
+
+  @IsOptional()
+  @IsString()
+  expiresAt?: string | null;
 }
